79572bd15612ff6419fbc62c1b25b6690ed35851
r3hab-media <jared.newnam@gmail.com>
2022-09-01 23:00:48 -0700

    added GA, accessibility updates

--- public/js/app.js
+++ public/js/app.js
@@ -91,11 +91,21 @@ const settings = [
 	`A high school student is studying to become an engineer. One day, he's experiementing with a high-powered battery to create a better power source for his home. The battery explodes, sending shrapnel across his face, leaving a deep cut. As he's gathering himself and cleaning up, he hears a knock at his front door. It's a man twice his age with a healed, but matching scar.`,
 	`A boy stays home while his parents go out to dinner. The boy, 16, decides to take one of the other cars out with his friends. They buy some beer and the boy drinks while he drives with his friends. Suddenly, all the kids realize they've been in a car accident. They're upside down after rolling a few times. The boy gets out of the car to see what they hit. It was his parents as they were on their way home from their date.`,
 	`As a 16 year old girl in Afghanistan is whipped and beaten, by her father, for trying going to school, her anger manifests. She looks in the mirror at one of the bruises that nearly covers a birthmark on her face. She cries and  after finding herself home alone later that day, she goes through her father's things and finds a birth certificate with another girls's name on it and a birthdate within a month of her own. She has no access to the Internet at home, so she delivers the birth certificate to a friend of hers. After a brief web search, her friend reports that the person listed on the birth certificate was kidnapped from the backyard of her house, 14 years earlier, from a small town in Ohio, in the United States. Her friend brought a picture of the kidnapped girl... who has a matching birthmark.`,
+	`You lost your site sight... along with everyone else on Earth - in the Great Blinding. Two years later, without warning, your sight returns. As you look around, you realize that every available wall, floor and surface has been painted with the same message - <i><strong>DON'T TELL THEM YOU CAN SEE</strong></i>.`,
+	`After years of gentle persuasion, the best friend you've had since childhood finally agrees to see a professional therapist for some serious mental issues. As she begins to improve, you slowly start to realize that you are her imaginary friend.`,
+	`Your parents have always been protective of you for as long as you could remember. The entire house was padded, even when you grew out of your toddler years. You were home-schooled and someone was alwways hovering in the area to make sure you didn't trip or hurt yourself. You thought this was just overprotective parents until one January day. Your hands were so dry that your skin broke. A drop of blood fell to the ground and almost instantly, red flowers grew and bloomed from where the drop landed.`,
+	`When people are born, they are assigned a soulmate. The two soulmates are assigned a song that only they know. On a rainy night, you hear someone break in through the back door. They are wearing a mask and carrying a large knife. They are also humming your song under their breath.`,
+	`Everytime you fall in love, you lose a part of your sould to the person you love. Everytime someone flals in love with you, you get a piece of their soul. You die when you run out of soul to give. History only knows of two immortals so far... one who never found love and one who makes people fall in love with them in order to extend their life.`,
+	`Children who die at a young age are given a very special role in the afterlife: they become invisible friends and are assigned to other children who need them back in the living realm.`,
+	`Whenever you feel like someone is watchging you, you try to shrug it off. However, one night you can't shake the feeling that someone is right behind you, so you turn around and standing there in front of you, right in the doorway, is anything but imaginary. It's an imaginary friend you created as a child. They drop a note on the ground and fade into the light. The note says, "<i><strong>Get me out of here</strong></i>.`,
+	`You have the ability to mentally hear the honst answer to any question just by looking at a person and thinking the question. It was all fun and games until you looked in the mirror and asked a question you shouldn't have.`,
+	`Every baby is taken away by the government and returned when they are 10 years old. They never remember what happened in those years, but they always recognize their parents. You, however, are a different story. You remember everything that happened and the people you are sent to live with... they're not your parents.`,
+	`You are in a typical awkward elevator ride with a group of people you don't know. Suddenly a man says, "<i>So, I bet you're all wondering why I've gathered you here..."</i>, before hitting the ermgency button.`
 ];
 
 const randomSettingGenerator = () => {
 	let randomSetting = settings[Math.floor(Math.random() * settings.length)];
-	document.getElementById("setting").textContent = randomSetting;
+	document.getElementById("setting").innerHTML = randomSetting;
 };
 
 const themes = [
@@ -172,64 +182,63 @@ const archetypes = [
 	{
 		type: "The Hero",
 		desc: "The protagonist who rises to meet a challenge and saves the day.",
-		strenghts: "courage, perseverance, honor",
+		strengths: "courage, perseverance, honor",
 		weaknesses: "overconfidence, hubris",
 	},
 	{
 		type: "The Outlaw",
 		desc: "The rebel who won’t abide by society’s demands.",
-		strenghts: "independent thinking, virtue, owes no favors",
+		strengths: "independent thinking, virtue, owes no favors",
 		weaknesses: "self-involved, potentially criminal",
 	},
 	{
 		type: "The Explorer",
 		desc: "A character naturally driven to push the boundaries of the status quo and explore the unknown.",
-		strenghts: "curious, driven, motivated by self-improvement",
+		strengths: "curious, driven, motivated by self-improvement",
 		weaknesses: "restless, unreliable, never satisfied",
 	},
 	{
 		type: "The Sage",
 		desc: "A wise figure with knowledge for those who inquire. The mother figure or mentor is often based on this archetype.",
-		strenghts: "wisdom, experience, insight",
+		strengths: "wisdom, experience, insight",
 		weaknesses: "cautious, hesitant to actually join the action",
 	},
 	{
 		type: "The Innocent",
 		desc: "A morally pure character, often a child, whose only intentions are good.",
-		strenghts: "morality, kindness, sincerity",
+		strengths: "morality, kindness, sincerity",
 		weaknesses: "vulnerable, naive, rarely skilled",
 	},
 	{
 		type: "The Creator",
 		desc: "A motivated visionary who creates art or structures during the narrative.",
-		strenghts: "creativity, willpower, conviction",
+		strengths: "creativity, willpower, conviction",
 		weaknesses: "self-involvement, single-mindedness, lack of practical skills",
 	},
 	{
 		type: "The Ruler",
 		desc: "A character with legal or emotional power over others.",
-		strenghts: "omnipotence, status, resources",
+		strengths: "omnipotence, status, resources",
 		weaknesses: "aloofness, disliked by others, out of touch",
 	},
 	{
 		type: "The Caregiver",
 		desc: "A character who continually supports others and makes sacrifices on their behalf.",
-		strenghts: "honorable, selfless, loyal",
+		strengths: "honorable, selfless, loyal",
 		weaknesses: "lacking personal ambition or leadership",
 	},
 	{
 		type: "The Everyman",
 		desc: "A relatable character who feels recognizable from daily life.",
-		strenghts: "grounded, salt-of-the-earth, relatable",
+		strengths: "grounded, salt-of-the-earth, relatable",
 		weaknesses: "lacking special powers, often unprepared for what’s to come",
 	},
 	{
 		type: "The Jester",
 		desc: "A funny character or trickster who provides comic relief, but may also speak important truths.",
-		strenghts: "funny, disarming, insightful",
+		strengths: "funny, disarming, insightful",
 		weaknesses: "can be obnoxious and superficial",
-	},
-	//https://www.masterclass.com/articles/writing-101-the-12-literary-archetypes#Xd05Y8CaLcnECgVeJdSfZ
+	}
 ];
 
 const archetypesTemplate = (types) => {
